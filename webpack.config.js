e8c7ebfc8650cf5bf6722f2599ebb7cfcaf99874
pasierb <mpasierbski@gmail.com>
2017-08-17 14:40:00 +0200

    gh pages dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,18 @@
-const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'docs'),
   },
   module: {
     loaders: [
       { test: /\.js/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx/, loader: 'babel-loader', exclude: /node_modules/ }
-    ]
+      { test: /\.jsx/, loader: 'babel-loader', exclude: /node_modules/ },
+    ],
   },
   resolve: {
-    extensions: ['.js', '.jsx']
-  }
-}
+    extensions: ['.js', '.jsx'],
+  },
+};
